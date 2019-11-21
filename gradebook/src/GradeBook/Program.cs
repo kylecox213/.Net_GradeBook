8ddfb90bdc32f332307fc0fb325372c4125a9f5b
Kyle Cox <kylecox213@gmail.com>
2019-11-21 14:02:34 -0500

    Add files via upload

---
+++ gradebook/src/GradeBook/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace GradeBook
+{
+    class Program
+    {
+        //inside of the main method
+                static void Main(string[] args)
+        {
+            //interpolation
+           // Console.WriteLine("Hello " + args[0] + "!");
+           if(args.Length > 0)
+           {
+             Console.WriteLine($"Hello, {args[0]}!");
+           }
+           else
+           {
+               Console.WriteLine("What up, playa?!");
+           }
+        }
+    }
+}
