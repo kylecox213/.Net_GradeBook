32b01ad01a9672fbf4f8483c88526de3703827fb
Alex Cox <Acox@mapcom.com>
2019-12-11 07:35:13 -0500

    modified book, program and statistics file

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -1,21 +1,22 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 
 namespace GradeBook
+{
 
-{   // define a delegate for adding grades that takes object sender and event arguments parameters 
     public delegate void GradeAddedDelegate(object sender, EventArgs args);
 
-    //create a base class for building other types (student class, school class, employee class)
-    public class NamedObject
+    public class NamedObject 
     {
-        // (Crtl + .) and create a contructor for the named object
+        //create a base class for building other types (student class, school class, employee class)
         public NamedObject(string name)
         {
+            // (Crtl + .) and create a contructor for the named object
             Name = name;
         }
 
-        // create a propery with the name Name
+        // create a property with the name Name
         public string Name
         {
             // create public getters and setters that will allow users to set a name
@@ -24,132 +25,118 @@ namespace GradeBook
         }
     }
 
+    public interface IBook
+    {
+        void AddGrade(double grade);
+        Statistics GetStatistics();
+        string Name { get; }
+        event GradeAddedDelegate GradeAdded;
+    }
     // add state to the class Book definition using a field definition
     // add the public acces modifier to the book class
 
     // add a semi colon followed by the name of the base class to allow for inheriting the getter and setter to the book class
     // public class Book : NamedObject
 
-    public class Book : NamedObject
-
+    public abstract class Book : NamedObject, IBook
     {
         // add a constructor parameter that will require a string name when invoking the new book constructor
-        // add a semicolon, base, and paranthesis to access a contructor on the base class
-        public Book(string name) : base("")
+        // include a semicolon, base, and paranthesis to access a contructor on the base class
+        public Book(string name) : base(name)
         {
-            grades = new List<double>();
-            // use the this variable to make the distinction between the field name on the left and the parameter on the right
-            Name = name;
         }
 
-        // create a method that takes a char and letter parameter
-        public void AddLetterGrade (char letter)
-        {
-            // create a switch case statement that assigns a letter grade relative to the grade score
-            switch(letter)
-                {
-                    case 'A':
-                        AddGrade(90);
-                        break;
-
-                    case 'B':
-                        AddGrade(80);
-                        break;
-
-                    case 'C':
-                        AddGrade(70);
-                        break;
-
-                    case 'D':
-                        AddGrade(60);
-                        break;
-
-                    default:
-                        AddGrade(0);
-                        break;
-
-                }
-        }
-        public void AddGrade(double grade)
-        // declare a list of double named grades outside of the addGrade method to make a field instead of a local variable
-        {
-            if(grade <= 100 && grade >= 0)
-                {   // add the grades to this instance of gradebook
-                    grades.Add(grade);
-                    // if the grade added is not null...
-                    if(GradeAdded != null)
-                    {
-                        // call the event listener to announce a grade has been added 
-                        GradeAdded(this, new EventArgs());
-                    }
-                }
-                else
-                {
-                    // throw an error indicating an invalid....
-                    throw new ArgumentException($"Invalid {nameof(grade)}");
-                }
-        }
-
-        // define a delegate for an event for publishing a grade; the event will be considered a member of the field type GradeAddedDelegate
-        public event GradeAddedDelegate GradeAdded;
+        public abstract event GradeAddedDelegate GradeAdded;
+        public abstract void AddGrade(double grade);
 
         // public method named GetStatistics with an object return type "Statistics"
-        public Statistics GetStatistics()
+        public abstract Statistics GetStatistics();
+    }
+
+    public class DiskBook : Book
+    {
+        public DiskBook(string name) : base(name)
+        {
+        }
+
+        public override event GradeAddedDelegate GradeAdded;
+
+        public override void AddGrade(double grade)
+        {
+            using(var writer = File.AppendText($"{Name}.txt"))
+            {                
+                writer.WriteLine(grade);
+
+                if(GradeAdded != null)
+                {
+                    GradeAdded(this, new EventArgs());
+                }
+            }
+        }
+
+        public override Statistics GetStatistics()
         {
             var result = new Statistics();
-            // create a var result and set it to 0
-            result.Average = 0.0;
-            // create a variable for storing the maximum potential value 
-            result.High = double.MinValue;
-            // create a variable for storing the minimum potential value
-            result.Low = double.MaxValue;
 
-            // create a foreach loop to iterate through the grades array
-            for (var i = 0; i < grades.Count; i++)
+            using(var reader = File.OpenText($"{Name}.txt"))
             {
-                if(grades[i] == 42.1)
-                    {
-                        break;
-                    }
-            
-                result.Low = Math.Min(grades[i], result.Low);
-                result.High = Math.Max(grades[i], result.High);
-                result.Average += grades[i];
-            };
-     
-            // use the Count property to total the number of grades
-            // divide the total number of grades by the sum; which is stored in the result variable
-            
-            switch(result.Average)
+                var line = reader.ReadLine();
+                while(line != null)
                 {
-                  case var d when d >= 90.0:
-                    result.Letter = 'A';
-                    break;
-
-                  case var d when d >= 80.0:
-                    result.Letter = 'B';
-                    break;
-
-                  case var d when d >= 70.0:
-                    result.Letter = 'C';
-                    break;
-
-                  case var d when d >= 60.0:
-                    result.Letter = 'D';
-                    break;
-
-                  default:
-                    result.Letter = 'F';
-                    break;
+                    var number = double.Parse(line);
+                    result.Add(number);
+                    line = reader.ReadLine();
                 }
+            }
+
+            return result;
+        }
+    }
+
+    public class InMemoryBook : Book
+    {
+        public InMemoryBook(string name) : base(name)
+        {            
+            grades = new List<double>();
+            Name = name;
+        }
+         
+        public override void AddGrade(double grade)
+        {            
+            if(grade <= 100 && grade >= 0)
+            {                
+                grades.Add(grade);  
+                if(GradeAdded != null)
+                {
+                    GradeAdded(this, new EventArgs());
+                }
+            }
+            else
+            {
+               throw new ArgumentException($"Invalid {nameof(grade)}");
+            }            
+        }
+
+        public override event GradeAddedDelegate GradeAdded;
+
+        public override Statistics GetStatistics()
+        {
+            var result = new Statistics();
+            
+            for(var index = 0; index < grades.Count; index += 1)
+            {
+                result.Add(grades[index]);                            
+            }
+
             // write the the result to the console
             // done:
             return result;
         }
+
         // add the private access modifer to render the grades field and name field 
         // best practices recommend separating the public accessible items from the private accessible items 
         private List<double> grades;
-
+        
         public const string CATEGORY = "Science";
     }
 }
--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -5,57 +5,22 @@ using System.Collections.Generic;
 // programmers utilize the Program class within the global namespace (microsoft class libraries)
 namespace GradeBook
 {
-
-
-    class Program
+    class Program 
     {
         static void Main(string[] args)
         {
             //create a new instance of book and pass in a string name parameter
-            var book = new Book("Jimmy's Book");
+
+            IBook book = new DiskBook("Tortellius' Grade Book");
+
             // pass in grades using the addGrade method and call the showStats method defined in the book.cs file
-            
-
             // subscribe to the event listener
-            book.GradeAdded += OnGradeAdded; 
+            book.GradeAdded += OnGradeAdded;
 
-            
-            while (true)
-            {
-                Console.WriteLine ("Enter a grade or 'q' to quit");
-                var input = Console.ReadLine();
-
-                if (input == "q")
-                {
-                    break;
-                }
-
-                try
-                {
-                    var grade = double.Parse(input);
-                    book.AddGrade(grade);
-                }
-
-                catch (ArgumentException ex)
-                {
-                    Console.WriteLine(ex.Message);
-                }
-
-                catch (FormatException ex)
-                {
-                    Console.WriteLine(ex.Message);
-                }
-
-                finally
-                {
-                    Console.WriteLine("**");
-                }
-            }
+            EnterGrades(book);
 
             var stats = book.GetStatistics();
-            book.Name = "";
 
-            Console.WriteLine(Book.CATEGORY);
             Console.WriteLine($"For the book named {book.Name}");
             Console.WriteLine($"The lowest grade is {stats.Low}");
             Console.WriteLine($"The highest grade is {stats.High}");
@@ -63,21 +28,58 @@ namespace GradeBook
             Console.WriteLine($"The letter grade is {stats.Letter}");
         }
 
+        private static void EnterGrades(IBook book)
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter a grade or 'q' to quit");
+                var input = Console.ReadLine();
+
+                if (input == "q")
+                {
+                    break;
+                }
+                try
+                {
+                    var grade = double.Parse(input);
+                    book.AddGrade(grade);
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    Console.WriteLine("**");
+                }
+            }
+        }
+        
         // invoke a static method for handling the gradeAdded event to be invoked
         // when the book class raises the event; grade added to the gradebook
 
         // -----**static methods can only be touched by other members that are static**-----
         // -----**per the delegate signature, it must be void and contain the name and parameters in order to subscribe to the event
+
         static void OnGradeAdded(object sender, EventArgs e)
         {
             Console.WriteLine("A grade was added");
         }
-    }   
+
+    }
 }
+
 
 //----------------------------------------***** Pillars of OOP *****------------------------------------------
 
 // encapsulation - allows to hide details about code via methods, properties and access modifiers 
 // inheritance - reusable code across similar classes
 // polymorphism - allows for having objects of the same type yet behave differently
+
+
+
 
